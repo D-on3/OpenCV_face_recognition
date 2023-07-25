a839eefa9949626255c627a7bf31eab8fc9e854e
D-on3 <dnedyalkov396@gmail.com>
2023-07-25 14:06:53 +0300

    add .gitignore

--- face_recognition.py
+++ face_recognition.py
@@ -3,63 +3,56 @@ from random import randrange
 
 class FaceDetector:
     def __init__(self, cascade_file_path):
-        # Initialize the FaceDetector with the Haar cascade classifier XML file
-        self.trained_face_data = cv2.CascadeClassifier(cascade_file_path)
+        # Initialize the face cascade classifier
+        self.face_cascade = cv2.CascadeClassifier(cascade_file_path)
 
-    def detect_faces(self, image_path):
-        # Load the image and convert it to grayscale
-        img = cv2.imread(image_path)
-        grey_scaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-        # Use the Haar cascade classifier to detect faces in the grayscale image
-        face_coordinates = self.trained_face_data.detectMultiScale(grey_scaled_img, scaleFactor=1.1, minNeighbors=2, minSize=(80, 80))
-
-        return img, face_coordinates
+    def detect_faces(self, image):
+        # Convert the image to grayscale for face detection
+        grey_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # Detect faces using the cascade classifier
+        return self.face_cascade.detectMultiScale(grey_img, scaleFactor=1.1, minNeighbors=2, minSize=(80, 80))
 
 class FaceDrawer:
     def draw_rectangles(self, img, face_coordinates):
-        # Draw rectangles around the detected faces on the image
+        # Draw rectangles around the detected faces
         for x, y, w, h in face_coordinates:
-            cv2.rectangle(img, (x, y), (x + w, y + h), (randrange(255), randrange(255), randrange(255)), 1)
-
-        # Return the image with rectangles drawn around the faces
+            cv2.rectangle(img, (x, y), (x + w, y + h), (randrange(255), randrange(255), randrange(255)), 2)
         return img
 
 class LiveFaceRecognizer:
     def __init__(self, cascade_file_path):
-        # Initialize the LiveFaceRecognizer with the FaceDetector and FaceDrawer
+        # Initialize the face detector and face drawer
         self.face_detector = FaceDetector(cascade_file_path)
         self.face_drawer = FaceDrawer()
 
-        # Create a named window for the video stream with a specific size (e.g., 800x600)
-        cv2.namedWindow("Live Face Recognition", cv2.WINDOW_NORMAL)
-        cv2.resizeWindow("Live Face Recognition", 800, 600)
-
     def recognize_faces(self):
-        # Open a video stream from the default camera (usually the webcam)
+        # Open the webcam stream
         video_stream = cv2.VideoCapture(0)
+
         while True:
-            # Read a frame from the video stream
-            success_frame_read, frame = video_stream.read()
-            if not success_frame_read:
+            # Read a frame from the webcam
+            success, frame = video_stream.read()
+            if not success:
                 break
 
-            # Convert the frame to grayscale and detect faces using FaceDetector
-            greyscale_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            face_coordinates = self.face_detector.detect_faces(greyscale_img)
+            # Detect faces in the current frame
+            face_coordinates = self.face_detector.detect_faces(frame)
 
-            # Draw rectangles around the detected faces using FaceDrawer
+            # Draw rectangles around the detected faces
             frame_with_rectangles = self.face_drawer.draw_rectangles(frame, face_coordinates)
 
-            # Display the frame with the rectangles around the detected faces
-            cv2.imshow("Live Face Recognition", frame_with_rectangles)
+            # Display the result
+            cv2.imshow("Detected Faces", frame_with_rectangles)
 
-            # Check if the space key is pressed to stop the video stream
-            pressed_key = cv2.waitKey(1)
-            if pressed_key == 32:
+            # Stop the webcam stream if 'q' is pressed
+            if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
-        # Release the video stream and close the window
+            # Stop the webcam stream if space key is pressed
+            if cv2.waitKey(1) & 0xFF == ord(' '):
+                break
+
+        # Release the webcam and close all OpenCV windows
         video_stream.release()
         cv2.destroyAllWindows()
 
@@ -69,7 +62,8 @@ if __name__ == "__main__":
 
     # Detect faces and draw rectangles on the image using FaceDetector and FaceDrawer
     face_detector = FaceDetector(cascade_file_path)
-    img, face_coordinates = face_detector.detect_faces(image_path)
+    img = cv2.imread(image_path)
+    face_coordinates = face_detector.detect_faces(img)
 
     face_drawer = FaceDrawer()
     img_with_rectangles = face_drawer.draw_rectangles(img, face_coordinates)
@@ -79,6 +73,6 @@ if __name__ == "__main__":
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-    # Perform live face recognition using LiveFaceRecognizer
+    # Create the LiveFaceRecognizer instance and start the face recognition
     live_face_recognizer = LiveFaceRecognizer(cascade_file_path)
     live_face_recognizer.recognize_faces()
