774a5e65f2d803db650426e9d0fdba3d93330b14
D-on3 <dnedyalkov396@gmail.com>
2023-07-25 12:09:24 +0300

    adding working version

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# face_regonition
+# Facial Recognition with OpenCV
+
+This Python script performs facial recognition using OpenCV's Haar cascade classifier and limits false positives to improve accuracy. The code detects faces in an input image and draws rectangles around them. Randomly colored rectangles are used to distinguish multiple detected faces.
+
+## Requirements
+
+- Python 3.10
+- OpenCV (cv2) library (Install with `pip install opencv-python`)
+
+## Usage
+
+1. Ensure you have Python and the required libraries installed.
+
+2. Download or clone the repository.
+
+3. Place the image you want to process in the same directory as the `main.py` script.
+
+4. In the terminal or command prompt, navigate to the directory containing the `main.py` script.
+
+5. Run the script and provide the image filename as an argument:
+
+
+Replace `<image_filename>` with the name of your input image file.
+
+6. The script will display the image with rectangles around the detected faces.
+
+## Customization
+
+You can adjust the parameters of the `detectMultiScale` function in the `limit_false_positives` function to fine-tune the sensitivity of the detector and reduce false positives:
+
+- `scaleFactor`: Controls how much the image size is reduced at each image scale. Lower values make detection slower but more accurate.
+- `minNeighbors`: Specifies the number of neighbors a region should have to be considered as a face. Higher values reduce false positives but may miss some faces.
+- `minSize`: Specifies the minimum size of the detected face. Faces smaller than this size are ignored.
+
+Experiment with different parameter values to find the best configuration for your specific images.
+
+## Example
+
+The example usage of the `limit_false_positives` function is shown in the `__main__` block of the `facial_recognition.py` script. You can run the example by executing the script without any arguments.
+
--- main.py
+++ main.py
@@ -1,30 +1,49 @@
 import cv2
+from random import randrange
 
-''' Load some pre-trained data on face frontals from opencv 
-(har cascade algorithm)'''
-trained_face_data = cv2.CascadeClassifier(
-    'haarcascade_frontalface_default.xml')
-''' choose an image to detect face in '''
-img = cv2.imread("Face-Recognition.jpg")
+def limit_false_positives(image_path):
+    '''
+    Function to perform facial recognition with OpenCV and limit false positives.
 
-''' Convert to greyscale check the different filters  on
-https://www.geeksforgeeks.org/python-opencv-cv2-cvtcolor-method/
-https://docs.opencv.org/3.4/d8/d01/group__imgproc__color__conversions.html '''
-grey_scaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    Parameters:
+        image_path (str): Path to the input image for facial recognition.
 
-''' feed the data to the algorithm = detect faces
-detectMultiScale = no matter the scale it look to face similar shape
-'''
-face_coordinates = trained_face_data.detectMultiScale(grey_scaled_img)
-'''print coordinates of the face'''
-print(face_coordinates)
+    Returns:
+        None (displays the image with rectangles around detected faces).
+    '''
 
-''' Draw rectangles arround the faces'''
+    ''' Load some pre-trained data on face frontals from OpenCV 
+    (Haar cascade algorithm)'''
+    trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-for(x,y,w,h) in face_coordinates:
-    cv2.rectangle(img,(x,y),(x+h , y +h),(0,255,0),1)
+    ''' choose an image to detect face in '''
+    img = cv2.imread(image_path)
 
-cv2.imshow("My face detector", img)
-cv2.waitKey()
+    ''' Convert to grayscale for better face detection '''
+    grey_scaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-print("d-On3")
+    ''' Feed the data to the algorithm = detect faces
+    detectMultiScale = no matter the scale it looks for similar shape faces '''
+    # Detect faces in the grayscale image using Haar cascades.
+    # Tuning scaleFactor, minNeighbors, and minSize to reduce false positives.
+    face_coordinates = trained_face_data.detectMultiScale(grey_scaled_img, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    ''' Print coordinates of the detected faces '''
+    # Print the detected face coordinates (x, y, w, h).
+    print(face_coordinates)
+
+    ''' Draw rectangles around the faces
+    The indexes of the numpy array correspond to the faces that are detected '''
+    # Loop through the detected faces and draw rectangles around them.
+    # Randomly colored rectangles are drawn to distinguish multiple faces.
+    for x, y, w, h in face_coordinates:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (randrange(255), randrange(255), randrange(255)), 1)
+
+    # Display the result
+    cv2.imshow("Detected Faces", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    image_path = "Family-Picnic-88-1024x683.jpg"
+    limit_false_positives(image_path)
